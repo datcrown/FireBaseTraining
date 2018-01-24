f176ea152087bf722963d01ce55c200f51f5cad5
Quoc Dat <quocdat@MTQ.local>
2018-01-24 09:59:37 +0700

    first Commit

---
+++ FireBaseTraining/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireBaseTraining
+//
+//  Created by Quoc Dat on 1/24/18.
+//  Copyright © 2018 Quoc Dat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
